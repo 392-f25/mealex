713211d9972f6dc459267946cc2f4a4fdeeb2f4c
ianfs4 <ianfslater4@gmail.com>
2025-11-11 18:30:39 -0600

    separated incoming and outgoing invitations

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from '@tanstack/react-router';
 import { signOut, useAuthState, useDataQuery } from '../utilities/firebase';
-import { MessageSquare } from 'lucide-react';
+import { House, MessageSquare } from 'lucide-react';
 import { type Message } from '../types/Message';
 import { useState, useEffect, useRef } from 'react';
 
@@ -10,8 +10,6 @@ const TopBar = () => {
   const queryPath = user ? `/invitations/${user.uid}/messages` : 'no-user-path';
   const [messagesData] = useDataQuery(queryPath);
   const [userMessages, setUserMessages] = useState<Message[]>([]);
-  const popupRef = useRef<HTMLDivElement>(null);
-
 
   useEffect(() => {
     if (user && messagesData) {
@@ -31,35 +29,25 @@ const TopBar = () => {
   
   return (
     <div className="sticky top-0 z-10 flex items-center justify-between bg-gradient-to-b from-white to-white/98 px-5 py-5 shadow-sm">
-      <div className="flex items-center gap-4">
-        <div>
-          <h1 className="m-0 text-xl font-semibold">MealEx</h1>
-          <p className="m-0 mt-0.5 text-xs text-slate-500">
-            Network with peers over meals
-          </p>
-        </div>
+      <div className="flex items-center gap-2">
         <button
-        onClick={() => navigate({ to: '/'})}
-        className="rounded-lg border border-blue-600 bg-transparent px-3 py-2 text-sm font-semibold text-blue-600 transition cursor-pointer hover:bg-blue-100"
-      >
-        Home
-      </button>
+          onClick={() => navigate({ to: '/'})}
+          className="relative p-2 rounded-full hover:bg-gray-100"
+        >
+          <House className="h-6 w-6 cursor-pointer text-gray-600" />
+        </button>
+        <h1 className="m-0 text-xl font-semibold">MealEx</h1>
       </div>
-      
-
-      
 
       <div className='flex items-center gap-2'>
-        <div className="relative" ref={popupRef}>
-          <button onClick={() => navigate({ to: '/invitations'})} className="relative p-2 rounded-full hover:bg-gray-100">
-            <MessageSquare className="h-6 w-6 text-gray-600" />
-            {userMessages.filter(message => message.status == 'pending').length > 0 && (
-              <span className="absolute top-0 right-0 block h-5 w-5 rounded-full bg-red-500 text-white text-xs flex items-center justify-center">
-                {userMessages.filter(message => message.status == 'pending').length}
-              </span>
-            )}
-          </button>
-        </div>
+        <button onClick={() => navigate({ to: '/invitations'})} className="relative p-2 rounded-full cursor-pointer hover:bg-gray-100">
+          <MessageSquare className="h-6 w-6 text-gray-600" />
+          {userMessages.filter(message => message.status == 'pending').length > 0 && (
+            <span className="absolute top-0 right-0 block h-5 w-5 rounded-full bg-red-500 text-white text-xs flex items-center justify-center">
+              {userMessages.filter(message => message.status == 'pending').length}
+            </span>
+          )}
+        </button>
 
         <button
           onClick={handleManageProfile}
