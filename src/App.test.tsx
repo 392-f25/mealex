b51085d205538ba98cdbfccec5994e590bbe88d2
Eiko Reisz <eiko.reisz@gmail.com>
2025-12-01 15:45:06 -0600

    import fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,8 @@ import App from './App';
 
 describe('counter tests', () => {
     
-  test("Counter should be 0 at the start", () => {
+  test("MealEx", () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.getByText('MealEx')).toBeDefined();
   });
-
-  test("Counter should increment by one when clicked", async () => {
-    render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
-  });
-
 });
